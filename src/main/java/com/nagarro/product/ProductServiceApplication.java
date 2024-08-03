0593039df1c174adeb050903920ee587808f2dae
Rohit Rawat <rohit.rawat@nagarro.com>
2024-08-03 11:59:16 +0530

    package name refactor

--- src/main/java/com/nagarro/product/ProductServiceApplication.java
+++ src/main/java/com/nagarro/product/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nagarro.product_service;
+package com.nagarro.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nagarro/product/ProductServiceApplicationTests.java
+++ src/test/java/com/nagarro/product/ProductServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nagarro.product_service;
+package com.nagarro.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
